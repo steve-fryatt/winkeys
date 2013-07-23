c94ab838be9a1a28a915b3a587ef875c57b073e6
Steve Fryatt <info@stevefryatt.org.uk>
2013-07-23 23:16:38 +0000

    Begin conversion into ObjAsm format.

--- src/WinKey.s
+++ src/WinKey.s
@@ -1,128 +1,122 @@
-REM >WinKeySrc
-REM WinKeys Module
-REM
-REM Do something with the extra keys on the keyboard.
-REM
-REM (c) Stephen Fryatt, 2001
-REM
-REM Needs ExtBasAsm to assemble.
-REM 26/32 bit neutral
+; Copyright 2001-2013, Stephen Fryatt (info@stevefryatt.org.uk)
+;
+; This file is part of WinKeys:
+;
+;   http://www.stevefryatt.org.uk/software/
+;
+; Licensed under the EUPL, Version 1.1 only (the "Licence");
+; You may not use this work except in compliance with the
+; Licence.
+;
+; You may obtain a copy of the Licence at:
+;
+;   http://joinup.ec.europa.eu/software/page/eupl
+;
+; Unless required by applicable law or agreed to in
+; writing, software distributed under the Licence is
+; distributed on an "AS IS" basis, WITHOUT WARRANTIES
+; OR CONDITIONS OF ANY KIND, either express or implied.
+;
+; See the Licence for the specific language governing
+; permissions and limitations under the Licence.
 
-version$="0.21"
-save_as$="WinKeys"
+; WinKey.s
+;
+; WinKeys Module Source
+;
+; REM 26/32 bit neutral
 
-LIBRARY "<Reporter$Dir>.AsmLib"
+;version$="0.21"
+;save_as$="WinKeys"
 
-PRINT "Assemble debug? (Y/N)"
-REPEAT
- g%=GET
-UNTIL (g% AND &DF)=ASC("Y") OR (g% AND &DF)=ASC("N")
-debug%=((g% AND &DF)=ASC("Y"))
+	GET	$Include/SWINames
 
-ON ERROR PRINT REPORT$;" at line ";ERL : END
+; ---------------------------------------------------------------------------------------------------------------------
+; Set up the Module Workspace
 
-REM --------------------------------------------------------------------------------------------------------------------
-REM Set up workspace
+WS_BlockSize		*	256
+WS_TargetSize		*	&500
 
-workspace_size%=0 : REM This is updated.
+			^	0
+WS_TaskHandle		#	4
+WS_Quit			#	4
+WS_WinHandle		#	4
+WS_Block		#	WS_BlockSize
+WS_Stack		#	WS_TargetSize - @
 
-task_handle%=FNworkspace(workspace_size%,4)
-quit%=FNworkspace(workspace_size%,4)
-win_handle%=FNworkspace(workspace_size%,4)
-block%=FNworkspace(workspace_size%,256)
-
-stack%=FNworkspace(workspace_size%,1012)
-
-REM --------------------------------------------------------------------------------------------------------------------
-
-DIM time% 5, date% 256
-?time%=3
-SYS "OS_Word",14,time%
-SYS "Territory_ConvertDateAndTime",-1,time%,date%,255,"(%dy %m3 %ce%yr)" TO ,date_end%
-?date_end%=13
-
-REM --------------------------------------------------------------------------------------------------------------------
-
-code_space%=4000
-DIM code% code_space%
-
-IF debug% THEN PROCReportInit(200)
+WS_Size			*	@
 
 
-FOR pass%=%11100 TO %11110 STEP 2
-L%=code%+code_space%
-O%=code%
-P%=0
-IF debug% THEN PROCReportStart(pass%)
-[OPT pass%
-EXT 1
-          EQUD      task_code           ; Offset to task code
-          EQUD      init_code           ; Offset to initialisation code
-          EQUD      final_code          ; Offset to finalisation code
-          EQUD      service_code        ; Offset to service-call handler
-          EQUD      title_string        ; Offset to title string
-          EQUD      help_string         ; Offset to help string
-          EQUD      command_table       ; Offset to command table
-          EQUD      0                   ; SWI Chunk number
-          EQUD      0                   ; Offset to SWI handler code
-          EQUD      0                   ; Offset to SWI decoding table
-          EQUD      0                   ; Offset to SWI decoding code
-          EQUD      0                   ; MessageTrans file
-          EQUD      module_flags
+; ======================================================================================================================
+; Module Header
+
+	AREA	Module,CODE,READONLY
+	ENTRY
+
+ModuleHeader
+	DCD	TaskCode			; Offset to task code
+	DCD	InitCode			; Offset to initialisation code
+	DCD	FinalCode			; Offset to finalisation code
+	DCD	ServiceCode			; Offset to service-call handler
+	DCD	TitleString			; Offset to title string
+	DCD	HelpString			; Offset to help string
+	DCD	CommandTable			; Offset to command table
+	DCD	0				; SWI Chunk number
+	DCD	0				; Offset to SWI handler code
+	DCD	0				; Offset to SWI decoding table
+	DCD	0				; Offset to SWI decoding code
+	DCD	0				; MessageTrans file
+	DCD	ModuleFlags			; Offset to module flags
 
 ; ======================================================================================================================
 
-.module_flags
-          EQUD      1                   ; 32-bit compatible
+ModuleFlags
+	DCD	1				; 32-bit compatible
 
 ; ======================================================================================================================
 
-.title_string
-          EQUZ      "WindowsKeys"
-          ALIGN
+TitleString
+	DCB	"WindowsKeys",0
+	ALIGN
 
-.help_string
-          EQUS      "Windows Keys"
-          EQUB      9
-          EQUS      version$
-          EQUS      " "
-          EQUS      $date%
-          EQUZ      " © Stephen Fryatt, 2001"
-          ALIGN
+HelpString
+	DCB	"Windows Keys",9,$BuildVersion," (",$BuildDate,") ",169," Stephen Fryatt, 2001",0 ;-",$BuildDate:RIGHT:4,0
+	ALIGN
 
 ; ======================================================================================================================
 
-.command_table
-          EQUZ      "Desktop_WindowsKeys"
-          ALIGN
-          EQUD      command_desktop
-          EQUD      &00000000
-          EQUD      0
-          EQUD      0
+CommandTable
+	DCB	"Desktop_WindowsKeys",0
+	ALIGN
+	DCD	CommandDesktop
+	DCD	&00000000
+	DCD	0
+	DCD	0
 
-          EQUD      0
+	DCD	0
 
 ; ----------------------------------------------------------------------------------------------------------------------
 
-.command_desktop
-          STMFD     R13!,{R14}
+CommandDesktop
+	STMFD	R13!,{R14}
 
-          MOV       R2,R0
-          ADR       R1,title_string
-          MOV       R0,#2
-          SWI       "XOS_Module"
+	MOV	R2,R0
+	ADR	R1,title_string
+	MOV	R0,#2
+	SWI	XOS_Module
 
-          LDMFD     R13!,{PC}
+	LDMFD	R13!,{PC}
 
 ; ======================================================================================================================
-.init_code
+
+InitCode
           STMFD     R13!,{R14}
 
 ; Claim 296 bytes of workspace for ourselves and store the pointer in our private workspace.
 ; This space is used for everything; both the module 'back-end' and the WIMP task.
 
           MOV       R0,#6
-          MOV       R3,#workspace_size%
+          MOV       R3,#WS_Size
           SWI       "XOS_Module"
           BVS       init_exit
           STR       R2,[R12]
@@ -131,26 +125,27 @@ EXT 1
 ; Initialise the workspace that was just claimed.
 
           MOV       R0,#0
-          STR       R0,[R12,#task_handle%]         ; Zero the task handle.
+          STR       R0,[R12,#WS_TaskHandle]         ; Zero the task handle.
 
 .init_exit
           LDMFD     R13!,{PC}
 
 ; ----------------------------------------------------------------------------------------------------------------------
-.final_code
+
+FinalCode
           STMFD     R13!,{R14}
           LDR       R12,[R12]
 
 ; Kill the wimp task if it's running.
 
-          LDR       R0,[R12,#task_handle%]
+          LDR       R0,[R12,#WS_TaskHandle]
           CMP       R0,#0
           BLE       final_free_ws
 
-          LDR       R1,task
+          LDR       R1,Task
           SWI       "XWimp_CloseDown"
           MOV       R1,#0
-          STR       R1,[R12,#task_handle%]
+          STR       R1,[R12,#WS_TaskHandle]
 
 .final_free_ws
           TEQ       R10,#1
@@ -165,7 +160,7 @@ EXT 1
 
 ; ======================================================================================================================
 
-.service_code
+ServiceCode
           TEQ       R1,#&27
           TEQNE     R1,#&49
           TEQNE     R1,#&4A
@@ -180,117 +175,117 @@ EXT 1
           BNE       service_start_wimp
 
           MOV       R14,#0
-          STR       R14,[R12,#task_handle%]
+          STR       R14,[R12,#WS_TaskHandle]
           LDMFD     R13!,{PC}
 
 .service_start_wimp
           TEQ       R1,#46
           BNE       service_started_wimp
 
-          LDR       R14,[R12,#task_handle%]
+          LDR       R14,[R12,#WS_TaskHandle]
           TEQ       R14,#0
           MOVEQ     R14,#NOT-1
-          STREQ     R14,[R12,#task_handle%]
-          ADREQ     R0,command_desktop
+          STREQ     R14,[R12,#WS_TaskHandle]
+          ADREQ     R0,CommandDesktop
           MOVEQ     R1,#0
           LDMFD     R13!,{PC}
 
 .service_started_wimp
-          LDR       R14,[R12,#task_handle%]
+          LDR       R14,[R12,#WS_TaskHandle]
           CMN       R14,#1
           MOVEQ     R14,#0
-          STREQ     R14,[R12,#task_handle%]
+          STREQ     R14,[R12,#WS_TaskHandle]
           LDMFD     R13!,{PC}
 
 ; ======================================================================================================================
 
-.task
-          EQUS      "TASK"
+Task
+	DCB	"TASK"
 
-.wimp_version
-          EQUD      310
+WimpVersion
+	DCD	310
 
-.wimp_messages
-          EQUD      0
+WimpMessageList
+	DCD	0
 
-.poll_mask
-          EQUD      &3831
+PollMask
+	DCD	&3831
 
-.task_name
-          EQUZ      "Windows Keys"
+TaskName
+	DCB	"Windows Keys",0
 
-.misused_start_command
-          EQUD      0
-          EQUZ      "Use *Desktop to start WinKeys."
-          ALIGN
+MisusedStartCommand
+	DCD	0
+	DCB	"Use *Desktop to start WinKeys.",0
+	ALIGN
 
-.window_definition
-          EQUD      -400      ; Visible area min x
-          EQUD      -400      ; Visible area min y
-          EQUD      -100      ; Visible area max x
-          EQUD      -100      ; Visible area max y
-          EQUD      0         ; X scroll offset
-          EQUD      0         ; Y scroll offset
-          EQUD      -3        ; Handle to open window behind
-          EQUD      &84001050 ; Window flags
-          EQUD      &07020701
-          EQUD      &07020C00
-          EQUD      0         ; Work area min x
-          EQUD      -300      ; Work area min y
-          EQUD      300       ; Work area max x
-          EQUD      0         ; Work area max y
-          EQUD      &19       ; Title bar icon flags
-          EQUD      0         ; Work area flags
-          EQUD      1         ; Sprite area pointer
-          EQUW      0         ; Minimum width of window
-          EQUW      0         ; Minimum height of window
-          EQUS      "Win Key Grab"
-          EQUD      0         ; Number of icons
-          ALIGN
+WindowDefinition
+	DCD	-400		; Visible area min x
+	DCD	-400		; Visible area min y
+	DCD	-100		; Visible area max x
+	DCD	-100		; Visible area max y
+	DCD	0		; X scroll offset
+	DCD	0		; Y scroll offset
+	DCD	-3		; Handle to open window behind
+	DCD	&84001050	; Window flags
+	DCD	&07020701
+	DCD	&07020C00
+	DCD	0		; Work area min x
+	DCD	-300		; Work area min y
+	DCD	300		; Work area max x
+	DCD	0		; Work area max y
+	DCD	&19		; Title bar icon flags
+	DCD	0		; Work area flags
+	DCD	1		; Sprite area pointer
+	DCW	0		; Minimum width of window
+	DCW	0		; Minimum height of window
+	DCB	"Win Key Grab"
+	DCD	0		; Number of icons
+	ALIGN
 
 ; ======================================================================================================================
 
-.task_code
+TaskCode
           LDR       R12,[R12]
-          ADRW      R13,workspace_size%+4         ; Set the stack up.
+          ADRW      R13,WS_Size+4         ; Set the stack up.
 
 ; Check that we aren't in the desktop.
 
           SWI       "XWimp_ReadSysInfo"
           TEQ       R0,#0
-          ADREQ     R0,misused_start_command
+          ADREQ     R0,MisusedStartCommand
           SWIEQ     "OS_GenerateError"
 
 ; Kill any previous version of our task which may be running.
 
-          LDR       R0,[R12,#task_handle%]
+          LDR       R0,[R12,#WS_TaskHandle]
           TEQ       R0,#0
-          LDRGT     R1,task
+          LDRGT     R1,Task
           SWIGT     "XWimp_CloseDown"
           MOV       R0,#0
-          STRGT     R0,[R12,#task_handle%]
+          STRGT     R0,[R12,#WS_TaskHandle]
 
 ; Set the Quit flag to zero
 
-          STR       R0,[R12,#quit%]
+          STR       R0,[R12,#WS_Quit]
 
 ; (Re) initialise the module as a Wimp task.
 
-          LDR       R0,wimp_version
-          LDR       R1,task
-          ADR       R2,task_name
-          ADR       R3,wimp_messages
+          LDR       R0,WimpVersion
+          LDR       R1,Task
+          ADR       R2,TaskName
+          ADR       R3,WimpMessageList
           SWI       "XWimp_Initialise"
           SWIVS     "OS_Exit"
-          STR       R1,[R12,#task_handle%]
+          STR       R1,[R12,#WS_TaskHandle]
 
 ; Create the window and open it on the next poll.
 
-          ADR       R1,window_definition
+          ADR       R1,WindowDefinition
           SWI       "Wimp_CreateWindow"
-          STR       R0,[R12,#win_handle%]
+          STR       R0,[R12,#WS_WinHandle]
 
-          ADRW      R1,block%
+          ADRW      R1,WS_Block
 
           STR       R0,[R1,#0]
           SWI       "Wimp_GetWindowState"
@@ -300,12 +295,12 @@ EXT 1
 
 ; Set the variables from the choices file (an obey file).
 
-          BL        set_key_vars
+          BL        SetKeyVariables
 
 ; ----------------------------------------------------------------------------------------------------------------------
 
 .poll_loop
-          LDR       R0,poll_mask
+          LDR       R0,PollMask
           SWI       "Wimp_Poll"
 
 .poll_event_open_window
@@ -337,46 +332,46 @@ EXT 1
           LDR       R0,[R1,#16]
           TEQ       R0,#0
           MOVEQ     R0,#1
-          STREQ     R0,[R12,#quit%]
+          STREQ     R0,[R12,#WS_Quit]
 
 .poll_loop_end
-          LDR       R0,[R12,#quit%]
+          LDR       R0,[R12,#WS_Quit]
           TEQ       R0,#0
           BEQ       poll_loop
 
 ; ----------------------------------------------------------------------------------------------------------------------
 
 .close_down
-          LDR       R0,[R12,#task_handle%]
-          LDR       R1,task
+          LDR       R0,[R12,#WS_TaskHandle]
+          LDR       R1,Task
           SWI       "XWimp_CloseDown"
 
 ; Set the task handle to zero and die.
 
           MOV       R0,#0
-          STR       R0,[R12,#task_handle%]
+          STR       R0,[R12,#WS_TaskHandle]
 
           SWI       "OS_Exit"
 
 ; ======================================================================================================================
 
-.key_1
-          EQUZ      "WinKeys$Win"
-.key_2
-          EQUZ      "WinKeys$ShiftWin"
-.key_3
-          EQUZ      "WinKeys$CtrlWin"
-.key_4
-          EQUZ      "WinKeys$ShiftCtrlWin"
-.key_5
-          EQUZ      "WinKeys$Menu"
-.key_6
-          EQUZ      "WinKeys$ShiftMenu"
-.key_7
-          EQUZ      "WinKeys$CtrlMenu"
-.key_8
-          EQUZ      "WinKeys$ShiftCtrlMenu"
-          ALIGN
+Key1
+	DCB	"WinKeys$Win",0
+Key2
+	DCB	"WinKeys$ShiftWin",0
+Key3
+	DCB	"WinKeys$CtrlWin",0
+Key4
+	DCB	"WinKeys$ShiftCtrlWin",0
+Key5
+	DCB	"WinKeys$Menu",0
+Key6
+	DCB	"WinKeys$ShiftMenu",0
+Key7
+	DCB	"WinKeys$CtrlMenu",0
+Key8
+	DCB	"WinKeys$ShiftCtrlMenu",0
+	ALIGN
 
 ; ----------------------------------------------------------------------------------------------------------------------
 
@@ -387,42 +382,42 @@ EXT 1
 
           MOV       R2,#&1C0
           TEQ       R5,R2
-          ADREQ     R0,key_1
+          ADREQ     R0,Key1
           BEQ       key_press_found
 
           ADD       R2,R2,#1
           TEQ       R5,R2
-          ADREQ     R0,key_5
+          ADREQ     R0,Key5
           BEQ       key_press_found
 
           MOV       R2,#&1D0
           TEQ       R5,R2
-          ADREQ     R0,key_2
+          ADREQ     R0,Key2
           BEQ       key_press_found
 
           ADD       R2,R2,#1
           TEQ       R5,R2
-          ADREQ     R0,key_6
+          ADREQ     R0,Key6
           BEQ       key_press_found
 
           MOV       R2,#&1E0
           TEQ       R5,R2
-          ADREQ     R0,key_3
+          ADREQ     R0,Key3
           BEQ       key_press_found
 
           ADD       R2,R2,#1
           TEQ       R5,R2
-          ADREQ     R0,key_7
+          ADREQ     R0,Key7
           BEQ       key_press_found
 
           MOV       R2,#&1F0
           TEQ       R5,R2
-          ADREQ     R0,key_4
+          ADREQ     R0,Key4
           BEQ       key_press_found
 
           ADD       R2,R2,#1
           TEQ       R5,R2
-          ADREQ     R0,key_8
+          ADREQ     R0,Key8
           BEQ       key_press_found
 
           MOV       R0,R5
@@ -453,45 +448,29 @@ EXT 1
 ; ======================================================================================================================
 
 .key_file_run
-          EQUS      "Filer_Run "
+          DCB      "Filer_Run "
 
 .key_file
-          EQUZ      "Choices:WinKeys.SetKeys"
+          DCB      "Choices:WinKeys.SetKeys",0
           ALIGN
 
 ; ----------------------------------------------------------------------------------------------------------------------
 
-.set_key_vars
-          STMFD     R13!,{R0-R6,R14}
+SetKeyVariables
+	STMFD	R13!,{R0-R6,R14}
 
-          MOV       R0,#23
-          ADR       R1,key_file
-          SWI       "XOS_File"
-          LDMVSFD   R13!,{R0-R6,PC}
+	MOV	R0,#23
+	ADR	R1,key_file
+	SWI	XOS_File
+	LDMVSFD	R13!,{R0-R6,PC}
 
-          TEQ       R0,#1
-          LDMNEFD   R13!,{R0-R6,PC}
+	TEQ	R0,#1
+	LDMNEFD	R13!,{R0-R6,PC}
 
-          ADR       R0,key_file_run
-          SWI       "XOS_CLI"
+	ADR	R0,key_file_run
+	SWI	XOS_CLI
 
-          LDMFD     R13!,{R0-R6,PC}
-]
-IF debug% THEN
-[OPT pass%
-          FNReportGen
-]
-ENDIF
-NEXT pass%
+	LDMFD	R13!,{R0-R6,PC}
 
-SYS "OS_File",10,"<Basic$Dir>."+save_as$,&FFA,,code%,code%+P%
+	END
 
-END
-
-
-
-DEF FNworkspace(RETURN size%,dim%)
-LOCAL ptr%
-ptr%=size%
-size%+=dim%
-=ptr%
